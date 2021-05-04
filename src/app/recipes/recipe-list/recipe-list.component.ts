9d2b7dff4fe70196c7275566a4dbcb8fa5a6b35d
samtoby11 <s.m.toby11@gmail.com>
2021-05-03 20:16:40 -0500

    Day 4 Commit
    Committing work that incorporates newly learned component and databinding practices.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.disneyfoodblog.com/wp-content/uploads/2013/10/MacAndCheeseTop.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.disneyfoodblog.com/wp-content/uploads/2013/10/MacAndCheeseTop.jpg')
+    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.disneyfoodblog.com/wp-content/uploads/2013/10/MacAndCheeseTop.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
